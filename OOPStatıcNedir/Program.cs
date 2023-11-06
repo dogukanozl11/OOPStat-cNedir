cbd109b70ebd397bc76f22be7fa795dc37905b8d
Doğukan ÖZEL <dogukanozel692@gmail.com>
2023-11-06 23:54:41 +0300

    Add project files.

---
+++ OOPStatıcNedir/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPStatıcNedir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Static : Nesne örneği alınmadan ulaşabildiğimiz metot , field, sınıf..
+
+            //Stack bölge içerisinde pointer ımız oluştu..
+            ogrenci O1;
+            //Stack bölgede pointer oluştuğu zaman static olan bir yapıcı metot çalıştı
+            //ve yine class içerisinde tanımlı olan static field ,
+            //metot static dediğmiz bölgede oluşturuldu.
+            ogrenci.Test2();
+
+            O1 = new ogrenci();
+            //Stack bölgede oluşan pointer => kendisini heap bölgede tamamladı... Static olmayan tanınlarını tamamladı..
+            O1.Test1(); //nesne örneği alındıktan sonra test1 adındaki metotuma ulaşabildim.
+        }
+    }
+}
---
+++ OOPStatıcNedir/ogrenci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPStatıcNedir
+{
+    public class ogrenci //internal
+    {
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+        public string EmailAdres { get; set; }
+
+        public void Test1()
+        {
+            Console.WriteLine("Nesne örneği alındıktan sonra çalışan metot");
+        }
+        public static void Test2()
+        {
+            Console.WriteLine("Nesne örneği ALINMADAN kullanıla bilir.");
+        }
+    }
+}
